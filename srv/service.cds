79bd583ebf821410e1e27f8978e3a71b33a65421
C0681341 vale.com <c0681341>
2024-07-07 00:27:32 +0000

    job application entity updated

--- srv/service.cds
+++ srv/service.cds
@@ -2,13 +2,28 @@ using {JobApplicationExt} from './external/JobApplicationExt';
 
 service MyService @(path: '/proxy'){
  entity JobApplication as projection on JobApplicationExt.JobApplication {
+    applicationId,
+    candidateId,
     jobReqId,
     firstName,
     lastName,
     jobTitle,
     jobAppStatus: redirected to JobApplicationStatus,
     jobRequisition: redirected to JobRequisition,
-    statusId: redirected to PicklistOption
+    statusId: redirected to PicklistOption,
+    custom_parentePublico1, 
+    custom_parentePublico2, 
+    custom_parentePublico3,
+    custom_tituloPublico1, 
+    custom_tituloPublico2, 
+    custom_tituloPublico3,
+    custom_grauPublico1, 
+    custom_grauPublico2, 
+    custom_grauPublico3,
+    custom_localPublico1, 
+    custom_localPublico2, 
+    custom_localPublico3,
+
  };
 
  entity JobApplicationStatus as projection on JobApplicationExt.JobApplicationStatus;
--- srv/service.js
+++ srv/service.js
@@ -3,9 +3,10 @@ const cds = require('@sap/cds')
 module.exports = async (srv) => {
     const JobApplicationExt = await cds.connect.to('JobApplicationExt')
 
-    srv.on('READ', 'JobApplication', async (req)=>  await JobApplicationExt.run(req.query))
-    srv.on('READ', 'JobApplicationStatus', async (req)=> await JobApplicationExt.run(req.query))
-    srv.on('READ', 'JobRequisition', async (req)=> await JobApplicationExt.run(req.query))
-    srv.on('READ', 'PicklistOption', async (req)=> await JobApplicationExt.run(req.query))
-    srv.on('READ', 'JobRequisition', async (req)=> await JobApplicationExt.run(req.query))
+
+    srv.on('READ', 'JobApplication', async (req) => await JobApplicationExt.run(req.query))
+    srv.on('READ', 'JobApplicationStatus', async (req) => await JobApplicationExt.run(req.query))
+    srv.on('READ', 'JobRequisition', async (req) => await JobApplicationExt.run(req.query))
+    srv.on('READ', 'PicklistOption', async (req) => await JobApplicationExt.run(req.query))
+    srv.on('READ', 'JobRequisition', async (req) => await JobApplicationExt.run(req.query))
 }
